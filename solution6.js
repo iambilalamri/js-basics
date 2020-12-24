fc047d540399cbfdc99dd43c582ca6b537bc4448
Bilal AMRI <iambilalamri@gmail.com>
2020-12-24 15:10:37 +0100

    [feat-js-basics]-add interview function

--- solution6.js
+++ solution6.js
@@ -9,7 +9,6 @@ print(callback);
 
 //*****************************/
 
-
 const message = function () {
   console.log("This message is shown after 3 seconds");
 };
@@ -18,6 +17,46 @@ setTimeout(message, 3000);
 
 //*****************************/
 
-setTimeout(() => { 
-    console.log("This message is shown after 4 seconds");
+setTimeout(() => {
+  console.log("This message is shown after 4 seconds");
 }, 4000);
+
+//**************************** */
+
+function printSpecialAlphabet() {
+  alphabel = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  for (let i = 0; i <= alphabel.length - 1; i++) {
+    if (i % 2 === 0) console.log(alphabel[i]);
+  }
+}
+
+printSpecialAlphabet();
+
+//********************************/
